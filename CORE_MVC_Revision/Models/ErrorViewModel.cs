baba908c6ee786ae01631825838c8c6d49e4e362
Pankaj Kumar Maurya <pankajmaurya872691@gmail.com>
2023-06-18 18:31:47 +0530

    Add project files.

---
+++ CORE_MVC_Revision/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CORE_MVC_Revision.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
